dc5922bd9c87505c133c87373134da40154f61bc
Thilak A <thilaka1003@gmail.com>
2026-02-26 21:35:36 +0530

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,13 @@
+print("Hello Thilak")
+#Area of circle
+radius=float(input("Enter radius"))
+print("Radius",radius)
+area = 3.14 * radius * radius
+print("Area =",round(area,4))
+
+
+#celcius to fahrenit
+fahr=float(input("ENter fahrenit"))
+print("Fahrenit",fahr)
+cel=(fahr-32)*0.56
+print(round(cel,2))           
